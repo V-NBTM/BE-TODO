f18f165c97051237dee3cdc62e4eea745da64adb
ChangHyo Yoo <chyy0807@gmail.com>
2023-01-21 02:47:35 +0900

    230121_1

--- src/test/java/com/example/demo/controller/TodoControllerTest.java
+++ src/test/java/com/example/demo/controller/TodoControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static com.example.demo.dto.TodoDTO.*;
-import static org.springframework.mock.web.reactive.function.server.MockServerRequest.builder;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -27,12 +27,14 @@ class TodoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private static final String userId = "temporary-user";
-
     @Test
-    @DisplayName("createTodo 테스트")
+    @DisplayName("controller createTodo 테스트")
     void createTodo() throws Exception {
+        TodoDTO dto = new TodoDTO("id", "title", true);
 
+        TodoEntity entity = TodoDTO.toEntity(dto);
+
+        given(todoService.create(entity));
 
         mockMvc.perform(post("/todo"))
                 .andExpect(status().isOk())
@@ -45,8 +47,13 @@ class TodoControllerTest {
     }
 
     @Test
+    @DisplayName("controller selectTodo 테스트")
     void selectTodo() throws Exception {
-        String todo = "todo";
+        TodoDTO dto = new TodoDTO("id", "title", true);
+
+        TodoEntity entity = TodoDTO.toEntity(dto);
+
+        given(todoService.select(entity));
 
         mockMvc.perform(get("/todo"))
                 .andExpect(jsonPath("$.id").exists())
@@ -57,7 +64,13 @@ class TodoControllerTest {
     }
 
     @Test
+    @DisplayName("controller updateTodo 테스트")
     void updateTodo() throws Exception {
+        TodoDTO dto = new TodoDTO("id", "title", true);
+
+        TodoEntity entity = TodoDTO.toEntity(dto);
+
+        given(todoService.update(entity));
 
         mockMvc.perform(put("/todo"))
                 .andExpect(status().isOk())
@@ -69,11 +82,16 @@ class TodoControllerTest {
     }
 
     @Test
+    @DisplayName("controller deleteTodo 테스트")
     void deleteTodo() throws Exception {
-        String todo = "todo";
+        TodoDTO dto = new TodoDTO("id", "title", true);
+
+        TodoEntity entity = TodoDTO.toEntity(dto);
+
+        given(todoService.delete(entity));
 
         mockMvc.perform(delete("/todo"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(todo));
+                .andExpect(content().json("/todo"));
     }
 }
